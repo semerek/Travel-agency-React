096d238ddcc431623a629bbe4db127282b98c5ae
Marta Semrau <mm.semrau@gmail.com>
2020-09-25 16:39:22 +0200

    14.5 update

--- travel-agency/src/components/features/TripListOptions/TripListOptionsContainer.js
+++ travel-agency/src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
 import TripListOptions from './TripListOptions';
 import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import {getAllFilters, changeSearchPhrase, addTag, removeTag, changeDuration} from '../../../redux/filtersRedux';
 
 const mapStateToProps = state => ({
   tags: getAllTags(state),
@@ -10,6 +10,9 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  addTag: tag => dispatch(addTag(tag)),
+  removeTag: tag => dispatch(removeTag(tag)),
+  changeDuration: duration => dispatch(changeDuration(duration),)
   // TODO - add more dispatchers for other filters
 });
 

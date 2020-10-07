682f92d26359697e1a2894776ec85e7aa447f8cd
Marta Semrau <mm.semrau@gmail.com>
2020-10-07 15:37:04 +0200

    node-module_ deleted files

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,6 +5,33 @@ import PropTypes from 'prop-types';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
 
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
 const OrderForm = ({tripCost, options, setOrderOption}) => (
   
   <Row>
@@ -16,6 +43,8 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
     <Col xs={12}>
       <OrderSummary totalCost={tripCost} orderOptions={options} />
     </Col>
+    <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
+
   </Row>
 );
 
